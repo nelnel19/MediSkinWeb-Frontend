8c912c631d96ae76a0081cfce11768b4a5b32703
nelnel19 <arnelvergara1234@gmail.com>
2026-03-23 10:17:41 +0800

    analysis

--- src/pages/SkinAnalysis.jsx
+++ src/pages/SkinAnalysis.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import API from "../api/axios";
-import "..styles/skinanalysis.css"; // optional styles
+import "../styles/skinanalysis.css"; // optional styles
 
 const SkinAnalysis = () => {
   const [history, setHistory] = useState([]);
